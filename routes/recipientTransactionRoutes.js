b5b74fa6b60dafb0762aeb9245b6e126ecae6a84
abhaysingh2101 <singhabhay2101@gmail.com>
2025-11-12 15:39:38 +0530

    Update recipientTransactionRoutes.js

--- routes/recipientTransactionRoutes.js
+++ routes/recipientTransactionRoutes.js
@@ -2,13 +2,16 @@ const express = require("express");
 const router = express.Router();
 const recipientTransactionController = require("../controllers/recipientTransactionController");
 
-// Book food (plates not reduced)
+// Book food 
 router.post("/book", recipientTransactionController.bookFood);
 
-// Take food (plates reduced)
+//  Take food 
 router.post("/take", recipientTransactionController.takeFood);
 
-// Get all recipient transactions
+//  Get all recipient transactions
 router.get("/", recipientTransactionController.getAllRecipientTransactions);
+
+// List recipients who booked food for a specific NGO
+router.get("/list/:ngoId", recipientTransactionController.listRecipientsBooked);
 
 module.exports = router;
